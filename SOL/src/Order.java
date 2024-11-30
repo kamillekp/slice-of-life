35e220e5155f274d51c211db7d75745712df97b8
Kamille <kamille.kpimentel@gmail.com>
2024-11-30 16:22:42 -0300

    pastas imagens e telas

--- SOL/src/Order.java
+++ SOL/src/Order.java
@@ -1,20 +1,20 @@
 import java.util.ArrayList;
 
 public class Order {
-    private State Initialstate;
+    private State currentState;
     private ArrayList<Pizza> pizzas = new ArrayList<Pizza>();
     private boolean completed;
    
     public Order() {
-        this.Initialstate = new InitialState(this);
+        this.currentState = new InitialState(this);
         this.completed = false;
     }
 
     public void processOrder(){
-        this.Initialstate.processOrder();
+        this.currentState.processOrder();
     }
 
     public void setState(State state){
-        this.Initialstate = state;
+        this.currentState = state;
     }
 }
