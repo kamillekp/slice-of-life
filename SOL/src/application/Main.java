44027d1c1c09aea42c656583634ab2ac1ebc63c1
Kamille <kamille.kpimentel@gmail.com>
2024-12-06 18:41:21 -0300

    algumas mudanças nas telas

--- SOL/src/application/Main.java
+++ SOL/src/application/Main.java
@@ -8,7 +8,6 @@ import javafx.scene.Scene;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 
- 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
